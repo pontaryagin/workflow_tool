a5c6fb8f89195b2354d992a4817132994cb4fe64
pontaryagin <ponta1496@gmail.com>
2024-02-07 13:29:18 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -14,31 +14,31 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
-import { Workflow } from "./workflow"
+import { WorkflowMain } from "./workflow"
+import { getWorkflow } from "@/app/model"
 
 async function submitAction(formData: FormData) {
   "use server"
   const name = formData.get("name")?.toString()
-  if (name)
-  {
+  if (name) {
     cookies().set("name", name)
   }
 }
 
-async function logout(formData: FormData){
+async function logout(formData: FormData) {
   "use server"
   cookies().delete("name")
 }
 
-export function DropdownMenuCheckboxes({username}: {username: string}) {
+export function DropdownMenuCheckboxes({ username }: { username: string }) {
   return <>
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <Badge>{username}</Badge>
+        <Badge>{ username }</Badge>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>
-          <form action={logout}>
+          <form action={ logout }>
             <button type="submit">Logout</button>
           </form>
         </DropdownMenuLabel>
@@ -49,19 +49,20 @@ export function DropdownMenuCheckboxes({username}: {username: string}) {
 
 const UsernameForm = () => {
   return <>
-    <form action={submitAction}>
-      <Input type="text" name="name" placeholder="name" className="mb-2"/>
+    <form action={ submitAction }>
+      <Input type="text" name="name" placeholder="name" className="mb-2" />
       <Button type="submit">submit</Button>
     </form>
   </>
 }
 
-const Main = ({username}: {username: string}) => {
+const Main = async ({ username }: { username: string }) => {
+  const workflow = await getWorkflow(9)
   return <>
     <div className="flex flex-row-reverse">
-      <DropdownMenuCheckboxes username={username}/>
+      <DropdownMenuCheckboxes username={ username } />
     </div>
-    <Workflow {...{username}}/>
+    <WorkflowMain { ...{ username, workflow } } />
   </>
 }
 
@@ -69,9 +70,9 @@ export default async function Home() {
   const username = cookies().get("name")?.value
   return (
     <main className="flex flex-col justify-between p-12">
-      {username != null && username !== ""
-        ?<Main username={username}/>
-        :<UsernameForm/>
+      { username != null && username !== ""
+        ? <Main username={ username } />
+        : <UsernameForm />
       }
     </main>
   )
--- app/workflow.tsx
+++ app/workflow.tsx
@@ -1,15 +1,25 @@
-import { use } from "react"
-import { getWorkflow } from "./model"
+'use client'
 import { WorkflowChart } from "./workflow_chart"
 import { WorkflowTable } from "./workflow_table"
-import { PrismaClient } from "@prisma/client"
+import { Workflow } from "@/app/model"
+import { WorkflowContext } from "./context"
+import React from "react"
 
-import { useRouter } from 'next/navigation'
-import { WorkflowMain } from "./workflow_main"
 
-export const Workflow = async () => {
-  const workflow = await getWorkflow(9)
+export const WorkflowMain = ({ workflow }: { workflow: Workflow }) => {
+  const [workflow_, setWorkflow] = React.useState<Workflow>(workflow)
+  const context = {
+    workflow: workflow_,
+    setWorkflow: setWorkflow,
+  }
   return (
-    <WorkflowMain { ...{ workflow } } />
+    <div>
+      <WorkflowContext.Provider value={ context } >
+        <h1 className="mb-5">Workflow Table</h1>
+        <WorkflowTable />
+        <h1 className="mt-10 mb-5">Workflow Chart</h1>
+        <WorkflowChart />
+      </WorkflowContext.Provider>
+    </div >
   )
 }
