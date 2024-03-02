0257296fd195ab099179b8f918b253209c5d3649
pontaryagin <ponta1496@gmail.com>
2024-03-02 17:25:20 +0900

    add descsription

--- app/workflow/[id]/workflow.tsx
+++ app/workflow/[id]/workflow.tsx
@@ -5,6 +5,17 @@ import { User, Workflow } from "@/lib/model"
 import { WorkflowContext } from "../../context"
 import React from "react"
 
+const WorkflowDescription = ({ workflow }: { workflow: Workflow }) => {
+  return <div>
+    <h1 className="mb-5 text-large">{ }</h1>
+    <h1 className="text-4xl font-extrabold tracking-tight">
+      { workflow.name }
+    </h1>
+    <p className="mt-5">
+      { workflow.description ? workflow.description : "No information" }
+    </p>
+  </div>
+}
 
 export const WorkflowMain = ({ workflow, currentUser }: { workflow: Workflow, currentUser: User }) => {
   const [workflow_, setWorkflow] = React.useState<Workflow>(workflow)
@@ -16,7 +27,8 @@ export const WorkflowMain = ({ workflow, currentUser }: { workflow: Workflow, cu
   return (
     <div>
       <WorkflowContext.Provider value={ context } >
-        <h1 className="mb-5">Workflow Table</h1>
+        <WorkflowDescription workflow={ workflow_ } />
+        <h1 className="mt-10 mb-5">Workflow Table</h1>
         <WorkflowTable />
         <h1 className="mt-10 mb-5">Workflow Chart</h1>
         <WorkflowChart />
